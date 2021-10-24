9fdebaa16026d09d56856f85bc89e433f81f4fdd
Nico Wipfler <nicowipfler@gmail.com>
2021-10-24 10:24:09 +0200

    - Modified forecasting for temp and wind in order to include all lead_times - Added script for creation of .csv-file - Uploaded the provided script for checking the format of this file - Included the forecast-folder in order to secure my forecasts

---
+++ check_submission.R
@@ -0,0 +1,188 @@
+# Instructions: Replace the path in read.csv with the path to the file you want to check
+# Then run the rest of the script. If error messages appear try to fix your file and re-try.
+# If the file is formally correct, only a series of statements on the checked properties
+# will be printed.
+# in case of problems email johannes.bracher@kit.edu
+
+dat <- read.csv(paste0(out_dir,"20211024_Obi_Wan_Kenobi.csv"))
+
+col_names <- colnames(dat)
+expected_colnames <- c("forecast_date", "target", "horizon", "q0.025", "q0.25", "q0.5", "q0.75", "q0.975")
+cols_quantiles <- c("q0.025", "q0.25", "q0.5", "q0.75", "q0.975")
+
+# check number of columns
+if(length(col_names) != length(expected_colnames)){
+  stop("data contain ", length(col_names), " columns, required are ", length(expected_colnames), " columns.")
+}else{
+  cat("checked: number of columns correct.\n")
+}
+
+# check column names
+if(any(col_names != expected_colnames)){
+  stop("At least one column name is not as expected or in the wrong order.
+       Column names should be: ", paste(expected_colnames, collapse = ", "))
+}else{
+  cat("checked:  column names correct.\n")
+}
+
+# check data types:
+
+dat$forecast_date <- as.Date(dat$forecast_date, format = "%Y-%m-%d")
+if(any(is.na(dat$forecast_date))){
+  stop("forecast_date needs to be date in format YYYY-MM-DD")
+}else{
+  cat("checked: date format correct.")
+}
+
+if(!is.character(dat$target)){
+  stop("target need to be a character string.")
+}else{
+  cat("checked: correct data type in column 'target' (string).")
+}
+
+if(!is.character(dat$horizon)){
+  stop("horizon need to be a character string.")
+}else{
+  cat("checked: correct data type in column 'horizon' (string).")
+}
+
+if(!is.numeric(dat$q0.025)){
+  stop("q0.025 need to be numeric.")
+}else{
+  cat("checked: Correct data type in column 'q0.025' (numeric).")
+}
+
+if(!is.numeric(dat$q0.25)){
+  stop("q0.25 need to be numeric.")
+}else{
+  cat("checked: correct data type in column 'q0.25' (numeric).")
+}
+
+if(!is.numeric(dat$q0.5)){
+  stop("q0.5 need to be numeric.")
+}else{
+  cat("checked: correct data type in column 'q0.5' (numeric).")
+}
+
+if(!is.numeric(dat$q0.75)){
+  stop("q0.75 need to be numeric.")
+}else{
+  cat("checked:  correct data type in column 'q0.75' (numeric).")
+}
+
+if(!is.numeric(dat$q0.975)){
+  stop("q0.975 need to be numeric.")
+}else{
+  cat("checked: Correct data type in column 'q0.975' (numeric).")
+}
+
+# check date is the same value in all rows
+if(length(unique(dat$forecast_date)) > 1){
+  stop("forecast_date needs to be the same in all rows.")
+}else{
+  cat("checked: all entries in forecast_date column are the same.")
+}
+
+# check date is not in the past
+if(dat$forecast_date[1] < Sys.Date()){
+  warning("Forecast_date should not be in the past.")
+}else{
+  cat("checked: forecast_date is not in the past.")
+}
+
+# check forecast_date is a Wednesday
+if(!weekdays(dat$forecast_date[1]) %in% c("Wednesday", "Mittwoch")){
+  stop("forecast_date needs to be a Wednesday.")
+}else{
+  cat("checked: forecast_date is a Wednesday.")
+}
+
+# check only allowed targets are there
+if(!all(dat$target %in% c("DAX", "temperature", "wind"))){
+  stop("target can only contain values 'DAX', 'temperature' and 'wind'")
+}else{
+  cat("checked: target only contains allowed values.")
+}
+
+# check entries where target is "DAX"
+sub_DAX <- subset(dat, target == "DAX")
+
+if(nrow(sub_DAX) != 5){
+  stop("Exactly 5 rows need to have target == 'DAX")
+}else{
+  cat("checked: correct number of forecasts for DAX.")
+}
+
+if(any(sort(sub_DAX$horizon) != c("1 day", "2 day", "5 day", "6 day", "7 day"))){
+  stop("For target == 'DAX', horizons need to be '1 day', '2 day', '5 day', '6 day', '7 day'")
+}else{
+  cat("checked: horizons for DAX are correct.")
+}
+
+# plausibility of DAX values:
+if(any(sub_DAX[, cols_quantiles] > 20 | sub_DAX[, cols_quantiles] < -20)){
+  warning("Implausible values for DAX detected. You may want to re-check them.")
+}else{
+  cat("checked: no major implausibilities in values for DAX.")
+}
+
+# check entries where target is "temperature"
+sub_temperature <- subset(dat, target == "temperature")
+
+# completeness:
+if(nrow(sub_temperature) != 5){
+  stop("Exactly 5 rows need to have target == 'temperature")
+}else{
+  cat("checked: correct number of forecasts for temperature")
+}
+
+# horizons of temperature forecasts:
+if(any(sort(sub_temperature$horizon) != c("36 hour", "48 hour", "60 hour", "72 hour", "84 hour"))){
+  stop("For target == 'temperature', horizons need to be '36 hour', '48 hour', '60 hour', '72 hour', '84 hour'")
+}else{
+  cat("checked: horizons for DAX are correct.")
+}
+
+# plausibility of temperature values:
+if(any(sub_temperature[, cols_quantiles] > 50 | sub_temperature[, cols_quantiles] < -30)){
+  warning("Implausible values for temperature detected. You may want to re-check them.")
+}else{
+  cat("checked: no major implausibilities in values for temperature.")
+}
+
+# check entries where target is "wind"
+sub_wind <- subset(dat, target == "wind")
+
+# completeness:
+if(nrow(sub_wind) != 5){
+  stop("Exactly 5 rows need to have target == 'wind")
+}else{
+  cat("checked: correct number of forecasts for wind.")
+}
+
+# horizons of wind forecasts:
+if(any(sort(sub_wind$horizon) != c("36 hour", "48 hour", "60 hour", "72 hour", "84 hour"))){
+  stop("For target == 'wind', horizons need to be '36 hour', '48 hour', '60 hour', '72 hour', '84 hour'")
+}else{
+  cat("checked: horizons for wind are correct.")
+}
+
+# plausibility of temperature values:
+if(any(sub_wind[, cols_quantiles] > 300 | sub_wind[, cols_quantiles] < 0)){
+  warning("Implausible values for wind detected. You may want to re-check them.")
+}else{
+  cat("checked: no major implausibilities in values for wind.")
+}
+
+# helper function to check if quantiles are ordered
+is_ordered <- function(x) all(diff(x) >= 0)
+
+# check that quantiles are ordered:
+for(i in 1:nrow(dat)){
+  if(!is_ordered(unlist(dat[i, cols_quantiles]))){
+    stop("Predictive quantiles in row ", i, " are not ordered correctly (need to be non-decreasing).")
+  }else{
+    if(i == nrow(dat)) cat("checked: all quantiles ordered correctly.")
+  }
+}
+
--- model_temp.R
+++ model_temp.R
@@ -1,108 +1,66 @@
 # Preparations ------------------------------------------------------------
 
+
 library(scoringRules)
 library(lubridate)
 library(crch)
 quantile_levels = c(0.025,0.25,0.5,0.75,0.975)
 
+
 # Load Data ---------------------------------------------------------------
 
+
+# Get historical data
 data_dir = "C://dev//Forecasting_Challenge//data//weather_historical//"
 data_dir_daily = "C://dev//Forecasting_Challenge//data//weather_daily//"
 load(paste0(data_dir, "icon_eps_t_2m.RData"))
 t2m_data_raw = data_icon_eps
 rm(data_icon_eps)
 
-# Examine Data ------------------------------------------------------------
-
-head(t2m_data_raw)
-range(t2m_data_raw$init_tm)
-
-lead_time = 48
-t2m_data = subset(t2m_data_raw, fcst_hour == lead_time)[!is.na(t2m_data$obs),]
-
-# Evaluate raw ensemble predictions ---------------------------------------
-
-t2m_ensfc_matrix <- as.matrix(t2m_data[,7:46])
-t2m_ens_crps <- crps_sample(y = t2m_data$obs, dat = t2m_ensfc_matrix)
-# this yields the CRPS of the ensemble forecasts (something like MSE)
-summary(t2m_ens_crps)
-
-t2m_ens_vrh <- sapply(seq_along(1:nrow(t2m_ensfc_matrix)),
-                      function(i) rank(c(t2m_data$obs[i], t2m_ensfc_matrix[i,]))[1])
-hist(t2m_ens_vrh, nclass = 41, freq = F, main="Verification Rank Histogram")
-# because the histogram is not flat at all, we can conclude that post-processing is needed
-
-# Fit EMOS model ----------------------------------------------------------
-
-t2m_data$ens_sd = sqrt(t2m_data$ens_var)
-# split data for training and testing
-t2m_data_train = subset(t2m_data, init_tm <= "2020-09-24")
-t2m_data_test = subset(t2m_data, init_tm >= "2020-09-25")
-# EMOS model
-t2m_model = crch(obs ~ ens_mean|ens_sd, # model formula
-                  data = t2m_data_train, # dataset
-                  dist = "gaussian", # parametric forecast distribution
-                  link.scale = "log", # link function for scale parameter
-                  type = "crps") # loss function
-t2m_model
-# evaluate ensemble predictions on test set
-t2m_ensfc_matrix_test = as.matrix(t2m_data_test[,7:46])
-t2m_ens_crps_test = crps_sample(y = t2m_data_test$obs, dat = t2m_ensfc_matrix_test)
-mean(t2m_ens_crps_test)
-# evaluate EMOS model on test set
-t2m_pred_loc = predict(t2m_model,
-                        t2m_data_test,
-                        type = "location")
-t2m_pred_sc = predict(t2m_model,
-                       t2m_data_test,
-                       type = "scale")
-t2m_model_crps_test = crps(y = t2m_data_test$obs, family = "norm", mean = t2m_pred_loc, sd = t2m_pred_sc)
-mean(t2m_model_crps_test)
-
-# Assess Calibration on test data set -------------------------------------
-
-t2m_ens_vrh_test = sapply(seq_along(1:nrow(t2m_ensfc_matrix_test)),
-                           function(i) rank(c(t2m_data_test$obs[i], t2m_ensfc_matrix_test[i,]))[1])
-t2m_model_pit_test = pnorm(t2m_data_test$obs, t2m_pred_loc, t2m_pred_sc)
-par(mfrow=c(1,2))
-hist(t2m_ens_vrh_test, nclass = 41, freq = F, main="Raw Ensemble Predictions"); abline(h = 1/41, lty = 2)
-hist(t2m_model_pit_test, nclass = 41, freq = F, ylim = c(0,6), main="EMOS Model"); abline(h = 1, lty = 2)
-
-###########################################################################
-#TODO Automatisiert für alle forecast horizons (examine data gehört dazu)
-
-# Estimate Final Model ----------------------------------------------------
-
-t2m_benchmark2 = crch(obs ~ ens_mean|ens_sd,
-                       data = t2m_data,
-                       dist = "gaussian",
-                       link.scale = "log",
-                       type = "crps")
-
-# Get Current Ensemble Forecasts ------------------------------------------
-
-#TODO Hier automatisiert das Datum anpassen?
+# Get current ensemble forecasts
+# TODO CHANGE DATE when current file has been downloaded to the corresponding folder
 new_fcst = read.table(file = paste0(data_dir_daily, "icon-eu-eps_2021102300_t_2m_Karlsruhe.txt"),
-                       sep = "|", header = TRUE)
+                      sep = "|", header = TRUE)
 # get rid of empty first and last row
 new_fcst[,1] = NULL
 new_fcst[,ncol(new_fcst)] = NULL
-# extract forecasts for targeted horizon
-ens_fc = new_fcst[new_fcst$fcst_hour == lead_time,][2:ncol(new_fcst)]
-ens_fc = as.numeric(ens_fc)
 
-# Forecast ----------------------------------------------------------------
 
-# baseline model
-t2m_benchmark1_pred = quantile(ens_fc, quantile_levels)
-# EMOS model
-pred_df = data.frame(ens_mean = mean(ens_fc), ens_sd = sd(ens_fc))
-t2m_benchmark2_loc = predict(t2m_benchmark2,
+# Model -------------------------------------------------------------------
+
+
+for (lead_time in c(36,48,60,72,84)){
+  
+  # create dataset with ensemble predictions and observations corresponding to current lead time
+  t2m_data = subset(t2m_data_raw, fcst_hour == lead_time)
+  t2m_data = t2m_data[!is.na(t2m_data$obs),]
+  t2m_data$ens_sd = sqrt(t2m_data$ens_var)
+  
+  # evaluate model on full historic data (with corresponding lead_time)
+  t2m_benchmark2 = crch(obs ~ ens_mean|ens_sd,
+                        data = t2m_data,
+                        dist = "gaussian",
+                        link.scale = "log",
+                        type = "crps")
+  
+  # extract current forecasts for targeted lead_time
+  ens_fc = new_fcst[new_fcst$fcst_hour == lead_time,][2:ncol(new_fcst)]
+  ens_fc = as.numeric(ens_fc)
+  
+  # forecast with baseline model
+  t2m_benchmark1_pred = quantile(ens_fc, quantile_levels)
+  
+  # forecast with EMOS model
+  pred_df = data.frame(ens_mean = mean(ens_fc), ens_sd = sd(ens_fc))
+  t2m_benchmark2_loc = predict(t2m_benchmark2,
+                               pred_df,
+                               type = "location")
+  t2m_benchmark2_sc = predict(t2m_benchmark2,
                               pred_df,
-                              type = "location")
-t2m_benchmark2_sc = predict(t2m_benchmark2,
-                             pred_df,
-                             type = "scale")
-t2m_benchmark2_pred = qnorm(quantile_levels, t2m_benchmark2_loc, t2m_benchmark2_sc)
-rbind(t2m_benchmark1_pred,t2m_benchmark2_pred)
+                              type = "scale")
+  t2m_benchmark2_pred = qnorm(quantile_levels, t2m_benchmark2_loc, t2m_benchmark2_sc)
+  
+  # print forecasts made by both models
+  print(paste("lead_time:",lead_time,"h"))
+  print(rbind(t2m_benchmark1_pred,t2m_benchmark2_pred))
+}
