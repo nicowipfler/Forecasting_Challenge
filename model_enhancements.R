77f5af8493a7d182ab3467b1f41c4e74e0bfe655
Nico Wipfler <nicowipfler@gmail.com>
2021-11-06 11:59:47 +0100

    First GARCH model - Experimented with different GARCH models - Result: GARCH(1,1) including ARMA(1,1) using student distribution works best - Forecasts can be made - Model will be implemented as function in order to be modularized and used flexibly soon - Created Plots and Statistics that can be included in the submission / elaboration:    - Plot of in_sample fit (mean and quantiles) and real observations    - Relative coverage frequencies of fitted confidence intervals (really good btw!) in sample    - Goodness of Fit statistics of different models (with or without ARMA, student or normal distribution)

--- model_enhancements.R
+++ model_enhancements.R
@@ -154,55 +154,154 @@ dev.off()
 # WEEK 3: GARCH for DAX ---------------------------------------------------
 
 
+# as common
 source('toolkit.R')
 load_libs(libs = c('dplyr', 'lubridate', 'tidyr', 'quantreg', 'scoringRules', 'crch', 'rdwd', 'ggplot2'))
 source('model_dax.R')
+# for garch model
+library('rugarch')
+# for qq plots
+library('car')
+#TODO Has to be added to lib list later!
+
+
+## Prepare Data
+
 
 dax_data = get_dax_data("2021-10-27")
 dax_data = dax_data[!is.na(dax_data$ret1),]
 ggplot(data = dax_data, aes(x = Date, y = ret1)) + geom_line()
 # Can be modelled as stationary time series, so ARMA modelling before GARCH is not necessary
-library('rugarch')
-#TODO Has to be added to lib list later!
+dax_df = dax_data[,c('Date','ret1')]
+dax_subset = subset(dax_df, as.Date(Date) > '2020-01-01')
+dax_df = data.frame(dax_subset[,'ret1'])
+rownames(dax_df) = dax_subset$Date
+
+
+## Erstes Test-Modell
+
+
 spec = ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
-                   mean.model = (list(armaOrder = c(0,0), include.mean = FALSE)),
-                   distribution.model = 'norm')
-#TODO Justify selection of student distribution
-spec
-#TODO Note optional parameter 'external.regressors'! Maybe useful?
-ugarch_fit = ugarchfit(spec, data = dax_data$ret1)
-ugarch_fit
-# Ljung-Box-Test zeigt, dass keine Autokorrelation zwischen Residuen mehr nachgewiesen werden kann -> Gut!
-# Zum Vergleich: ARMA(1,1)-model integrieren
-spec_arma = ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
                   mean.model = (list(armaOrder = c(1,1), include.mean = TRUE)),
                   distribution.model = 'norm')
-ugarch_arma_fit = ugarchfit(spec_arma, dat = dax_data$ret1)
-ugarch_arma_fit
-# Im Wesentlichen das Gleiche
-# Ich arbeite aus Gründen der Einfachheit mit GARCH ohne ARMA weiter
+ugarch_fit_test = ugarchfit(spec, data = dax_df)
+res_std_test = ugarch_fit_test@fit$residuals / ugarch_fit_test@fit$sigma
+qqPlot(res_std_test, dist='norm')
+# Normalverteilung scheint nicht ganz zu passen, versuche t-Verteilung
 
-# QQ Plot der Residuen
-res = ugarch_fit@fit$residuals
-qqnorm(scale(res))
-qqline(scale(res))
-# Verteilungsannahme scheint nicht zu passen
 
-# NEUES MODEL
+## DAS MODELL
+
+
 spec = ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
                   mean.model = (list(armaOrder = c(1,1), include.mean = TRUE)),
                   distribution.model = 'std')
-ugarch_fit = ugarchfit(spec, data = dax_data$ret1)
+# Modell schätzen
+ugarch_fit = ugarchfit(spec, data = dax_df)
 ugarch_fit
-#TODO Überprüfen, dass das besser passt?
+# Ljung-Box-Test: Statistisch kann keine Korrellation mehr in dne quadrierten, standardisierten Residuen nachgewiesen werden
+# Standardisierte Residuen:
+res_std = residuals(ugarch_fit, standardize = TRUE)
+# df der t-Vert. in Modellspezifikation
+t_df = ugarch_fit@fit$coef['shape']
+ugarch_fit@fit$coef
+# Problem: residuals()-Funktion liefert Daten mit, das wollen wir hier nicht. Also manuell:
+res_std_2 = ugarch_fit@fit$residuals / ugarch_fit@fit$sigma
+qqPlot(res_std_2, dist='t', df=t_df, ylim=c(-6,6))
+# Besser, aber noch nicht gut
+infocriteria(ugarch_fit_test)
+infocriteria(ugarch_fit)
+# Auch nach Informationskriterien (etwas) besser!
+# NOTE: Also ist student distr. besser geeignet als normal distr.
+# NOTE: ARMA(1,1)-Modell inkludiert, obwohl Zeitreihe beinahe stationär, da Modell mit ARMA besseren AIC, BIC, ... als ohne liefert. Siehe unten
+
+
+## FORECASTS
+
+
+ugarch_fc = ugarchforecast(ugarch_fit, n.ahead = 5, n.roll = 5)
+quantile(ugarch_fc, probs=0.025)
+quantile(ugarch_fc, probs=0.25)
+fitted(ugarch_fc)
+quantile(ugarch_fc, probs=0.75)
+quantile(ugarch_fc, probs=0.975)
+# Visualization
+plot(ugarch_fc)
+
+
+## Überprüfe relative Überdeckungshäufigkeiten für Forecasts innerhalb des Modells
+
+
+# GRAFISCH
+# ret1
+plot(dax_subset$ret1, type='l')
+# forecasted mean
+in_fit = ugarch_fit@fit$fitted.values
+points(in_fit, pch=16, col='blue', cex=0.2)
+# forecasted q0.025
+q0.025 = as.vector(quantile(ugarch_fit, 0.025))
+points(q0.025, pch=16, col='green', cex=0.15)
+# forecasted q0.25
+q0.25 = as.vector(quantile(ugarch_fit, 0.25))
+points(q0.25, pch=16, col='darkgreen', cex=0.15)
+# forecasted q0.75
+q0.75 = as.vector(quantile(ugarch_fit, 0.75))
+points(q0.75, pch=16, col='darkgreen', cex=0.15)
+# forecasted q0.975
+q0.975 = as.vector(quantile(ugarch_fit, 0.975))
+points(q0.975, pch=16, col='green', cex=0.15)
+# NUMERISCH
+count_vec_rel = function(vec, le=TRUE){
+  count = 0
+  if(le){
+    for (i in 1:length(vec)){
+      if(vec[i] < 0){
+        count = count+1
+      }
+    }
+  }
+  else if (ge){
+    for (i in 1:length(vec)){
+      if(vec[i] > 0){
+        count = count+1
+      }
+    }
+  }
+  return(count/length(vec))
+}
+# Relativer Anteil der Beobachtungen, die unter dem vorhergesagten 0.025 Quantil liegen
+count_vec_rel(dax_subset$ret1-q0.025)
+# Relativer Anteil der Beobachtungen, die unter dem vorhergesagten 0.25 Quantil liegen
+count_vec_rel(dax_subset$ret1-q0.25)
+# Relativer Anteil der Beobachtungen, die über dem vorhergesagten 0.75 Quantil liegen
+count_vec_rel(q0.75-dax_subset$ret1)
+# Relativer Anteil der Beobachtungen, die über dem vorhergesagten 0.975 Quantil liegen
+count_vec_rel(q0.975-dax_subset$ret1)
+# Insgesamt sind die Überdeckungshäufigkeiten der Konfidenzintervalle also SEHR gut:
+# Should be about 0.5
+count_vec_rel(dax_subset$ret1-q0.25) + count_vec_rel(q0.75-dax_subset$ret1)
+# Should be about 0.05
+count_vec_rel(dax_subset$ret1-q0.025) + count_vec_rel(q0.975-dax_subset$ret1)
+# Das Modell scheint also keine systematischen Fehler zu machen
+
+
+## (Test des Modells in einem Rolling Window Ansatz:)
+
 
 # Verwendung in Rolling Window Ansatz
-modelroll = ugarchroll(spec = spec, dax_data$ret1, n.ahead = 1, forecast.length = 10,
-                        refit.every = 1, refit.window = 'moving', window.size = 500, calculate.VaR = FALSE)
-# Vorhergesagte Erwartungswerte
-modelroll@forecast$density$Mu
-# Vorhergesagte Quantile
-quantile(modelroll, 0.025)
-quantile(modelroll, 0.25)
-quantile(modelroll, 0.75)
-quantile(modelroll, 0.975)
+modelroll = ugarchroll(spec = spec, dax_df, n.ahead = 1, forecast.length = 5, 
+                        refit.every = 1, refit.window = 'moving', window.size = 150, calculate.VaR = FALSE)
+# Hier lassen sich bei Mu die vorhergesagten Erwartungswerte und bei Realized die realisierten Werte ablesen
+modelroll
+
+
+## Test, ob ARMA modeling weglassen das Modell besser macht
+
+
+spec_test = ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,1)),
+                  mean.model = (list(armaOrder = c(0,0), include.mean = TRUE)),
+                  distribution.model = 'std')
+# Modell schätzen
+ugarch_fit_test = ugarchfit(spec_test, data = dax_df)
+infocriteria(ugarch_fit_test)
+# Mit ARMA minimal besser
