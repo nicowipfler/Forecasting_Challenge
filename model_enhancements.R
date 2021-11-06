fd4ef269d36e1772ede665dfaace6c9c2c0432f8
Nico Wipfler <nicowipfler@gmail.com>
2021-11-06 16:23:31 +0100

    Exploration for seasonal training data in EMOS - wrote test function emos_temp_test to test using seasonal training data - results don't differ massively - optimization of window still needs to be done - same thing should be tested for wind

--- model_enhancements.R
+++ model_enhancements.R
@@ -304,3 +304,70 @@ spec_test = ugarchspec(variance.model = list(model = 'sGARCH', garchOrder = c(1,
 ugarch_fit_test = ugarchfit(spec_test, data = dax_df)
 infocriteria(ugarch_fit_test)
 # Mit ARMA minimal besser
+
+
+# WEEK 3: Seasonal Data for EMOS ------------------------------------------
+
+
+# Prep
+source('toolkit.R')
+load_libs(libs = c('dplyr', 'lubridate', 'tidyr', 'quantreg', 'scoringRules', 'crch', 'rdwd', 'ggplot2','rugarch'))
+source('model_wind.R')
+source('model_temp.R')
+source('visual_checks.R')
+
+#### Testversion der Funktion temp_emos
+emos_temp_test = function(init_date, quantile_levels=c(0.025,0.25,0.5,0.75,0.975),season_days_around=0){
+  # prepare historical data
+  t2m_data_raw = get_hist_temp_data()
+  # Get current ensemble forecasts
+  data_dir_daily = "C://dev//Forecasting_Challenge//data//weather_daily//Berlin//"
+  date_formatted = gsub('-','',init_date)
+  new_fcst = read.table(file = paste0(data_dir_daily, "icon-eu-eps_",date_formatted,"00_t_2m_Berlin.txt"), sep = "|", header = TRUE)
+  # Get rid of empty first and last row
+  new_fcst[,1] = NULL
+  new_fcst[,ncol(new_fcst)] = NULL
+  # Prepare Output Data
+  fcst_temp = matrix(ncol = 5, nrow = 5)
+  # MODEL
+  i = 1
+  for (lead_time in c(36,48,60,72,84)){
+    # create dataset with ensemble predictions and observations corresponding to current lead time
+    t2m_data = subset(t2m_data_raw, fcst_hour == lead_time)
+    t2m_data = t2m_data[!is.na(t2m_data$obs),]
+    t2m_data$ens_sd = sqrt(t2m_data$ens_var)
+    # Only use seasonal data if season_days_around > 0
+    if(season_days_around>0){
+      lower = as.Date(init_date) - season_days_around
+      upper = as.Date(init_date) + season_days_around
+      t2m_data = subset(t2m_data, as.Date(init_tm) %in% lower:upper | 
+                          as.Date(init_tm) %in% (lower-365):(upper-365) | 
+                          as.Date(init_tm) %in% (lower-2*365):(upper-2*365)| 
+                          as.Date(init_tm) %in% (lower-3*365):(upper-3*365))
+    }
+    # evaluate model on full historic data (with corresponding lead_time)
+    t2m_benchmark2 = crch(obs ~ ens_mean|ens_sd,
+                          data = t2m_data,
+                          dist = "gaussian",
+                          link.scale = "log",
+                          type = "crps")
+    # extract current forecasts for targeted lead_time
+    ens_fc = new_fcst[new_fcst$fcst_hour == lead_time,][2:ncol(new_fcst)]
+    ens_fc = as.numeric(ens_fc)
+    # forecast with EMOS model
+    pred_df = data.frame(ens_mean = mean(ens_fc), ens_sd = sd(ens_fc))
+    t2m_benchmark2_loc = predict(t2m_benchmark2,
+                                 pred_df,
+                                 type = "location")
+    t2m_benchmark2_sc = predict(t2m_benchmark2,
+                                pred_df,
+                                type = "scale")
+    t2m_benchmark2_pred = qnorm(quantile_levels, t2m_benchmark2_loc, t2m_benchmark2_sc)
+    # Write to Output Data
+    fcst_temp[i,] = t2m_benchmark2_pred
+    i = i+1
+  }
+  return(fcst_temp)
+}
+emos_temp_test('2021-10-27',season_days_around = 40)
+emos_temp_test('2021-10-27',season_days_around = 0)
