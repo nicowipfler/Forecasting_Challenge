123520735e05e76907ec109d28977a4301c86f99
Nico Wipfler <nicowipfler@gmail.com>
2021-12-04 13:06:20 +0100

    Added functions for feature engineering QRF - one for train data (historical) - one for pred / test data (current)

--- toolkit.R
+++ toolkit.R
@@ -16,6 +16,7 @@ combine_forecasts = function(fc_in1, fc_in2, weights=c(0.5,0.5)){
   return(fc_out)
 }
 
+
 combine_many_forecasts = function(fc_array, weights=0){
   #' Function that combines any amount of forecasts by weighted averaging. Standard: arithmetic mean
   #' fc_array: array of matrices containing the forecasts
@@ -31,4 +32,44 @@ combine_many_forecasts = function(fc_array, weights=0){
   }
   fc_out
   return(fc_out)
+}
+
+
+qrf_feature_eng_train = function(df, lt){
+  #' Function that makes feature engineering for weather quantile regression forests training, returns ensemble statistics used for qrf
+  #' df: data frame containing the raw training data
+  #' lt: lead time we are currently training
+  
+  df_lt = subset(df, fcst_hour == lt)
+  df_lt$ens_sd = sqrt(df_lt$ens_var)
+  df_lt$ens_med = apply(df_lt[7:46], 1, median, na.rm=T)
+  df_lt$dez01 = apply(df_lt[7:46], 1, quantile, na.rm=T, probs= 0.1)
+  df_lt$dez09 = apply(df_lt[7:46], 1, quantile, na.rm=T, probs= 0.9)
+  df_lt$iqr = apply(df_lt[7:46], 1, IQR, na.rm=T)
+  df_lt$skew = apply(df_lt[7:46], 1, skewness, na.rm=T)
+  df_lt$kurt = apply(df_lt[7:46], 1, kurtosis, na.rm=T)
+  df_lt$mon = month(df_lt$obs_tm)
+  df_pred = select(df_lt, ens_mean, ens_med, ens_sd, dez01, dez09, iqr, skew, kurt, mon)
+  return(df_pred)
+}
+
+
+qrf_feature_eng_predict = function(df, lt, init_date){
+  #' Function that makes feature engineering for weather quantile regression forests predictions
+  #' df: data frame containing the raw training data
+  #' lt: lead time we are currently training
+  #' init_date: date on which the prediction is to be made
+  
+  df_lt = subset(df, fcst_hour == lt)
+  df_lt$ens_mean = apply(df_lt[3:42], 1, mean, na.rm=T)
+  df_lt$ens_sd = apply(df_lt[3:42], 1, sd, na.rm=T)
+  df_lt$ens_med = apply(df_lt[3:42], 1, median, na.rm=T)
+  df_lt$dez01 = apply(df_lt[3:42], 1, quantile, na.rm=T, probs= 0.1)
+  df_lt$dez09 = apply(df_lt[3:42], 1, quantile, na.rm=T, probs= 0.9)
+  df_lt$iqr = apply(df_lt[3:42], 1, IQR, na.rm=T)
+  df_lt$skew = apply(df_lt[3:42], 1, skewness, na.rm=T)
+  df_lt$kurt = apply(df_lt[3:42], 1, kurtosis, na.rm=T)
+  df_lt$mon = month(init_date)
+  df_pred = select(df_lt, ens_mean, ens_med, ens_sd, dez01, dez09, iqr, skew, kurt, mon)
+  return(df_pred)
 }
